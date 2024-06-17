c7338423e52fb2f1ed7738933d9c7faa42acfd62
petrovviacheslav <lery66lery@yandex.ru>
2024-06-18 01:17:29 +0300

    fix encoding x2

--- src/AsyncFileReader.java
+++ src/AsyncFileReader.java
@@ -7,31 +7,31 @@ import java.util.concurrent.Future;
 public class AsyncFileReader {
     public static void main(String[] args) {
         try {
-            // ��������� ���� � ����� � �������� ������ (�� ������)
+            // Указываем путь к файлу и параметр канала (на чтение)
             AsynchronousFileChannel fileChannel = AsynchronousFileChannel.open(Paths.get("file.txt"), StandardOpenOption.READ);
 
             ByteBuffer buffer = ByteBuffer.allocate(4096);
             Future<Integer> result = fileChannel.read(buffer, 0);
 
-            // ������ ���-�� ������ ���� �������� ������ �� ����� (�������� ���������)
+            // Делаем что-то другое пока читаются данные из файла (полезная программа)
             int k = 0;
             for (int i=0; i < 1000; i++){
                 k++;
             }
 
-            // ���� ���������� ������
+            // Ждём завершения чтения
             Integer bytesRead = result.get();
-            System.out.println("��������� ������: " + bytesRead);
+            System.out.println("Прочитано байтов: " + bytesRead);
 
-            // �������������� �����, ����� ��� ���������
+            // Переворачиваем буфер, чтобы его прочитать
             buffer.flip();
-            // ����, ���� � ������ ���� ��������
+            // Цикл, пока в буфере есть элементы
             while (buffer.hasRemaining()) {
                 System.out.print((char) buffer.get());
             }
             fileChannel.close();
         } catch (Exception e) {
-            System.out.println("��������� ������!");
+            System.out.println("Внезапна ошибка!");
         }
     }
 }
